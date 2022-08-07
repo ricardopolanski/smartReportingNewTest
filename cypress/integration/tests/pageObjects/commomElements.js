d3c699dc1b12a182f05ae54ff816e218de5d259c
Ricardo Polanski <rpolanski@live.com>
2022-08-07 19:01:40 -0300

    Developed page objects - commomElements - newComputerElements

---
+++ cypress/integration/tests/pageObjects/commomElements.js
@@ -0,0 +1,11 @@
+exports.CommomElements = class CommomElements{
+    constructor(){
+        this.message = '[class="alert-message warning"]';
+    }
+
+    confirmationMessage = () => {
+        return this.message
+    }
+    
+}
+
---
+++ cypress/integration/tests/pageObjects/newComputerElements.js
@@ -0,0 +1,35 @@
+exports.NewComputerElements = class NewComputerElements {
+    constructor(){
+        this.addComputerBtn = '#add';
+        this.newComputerField = '[id="name"]';
+        this.introcuced = '[id="introduced"]';
+        this.dicontinued = '[id="discontinued"]';
+        this.companyDropDown = '[id="company"]';
+        this.createComputerBtn = '[class="btn primary"]';
+    }
+
+    addBtn = () => {
+        return this.addComputerBtn
+    }
+    
+    computerfield = () => {
+        return this.newComputerField
+    }
+    
+    introcucedField = () => {
+        return this.introcuced
+    }
+    
+    dicontinuedField = () => {
+        return this.dicontinued
+    }
+    
+    companySelect = () => {
+        return this.companyDropDown
+    }
+    
+    createBtn = () => {
+        return this.createComputerBtn
+    }
+}
+
