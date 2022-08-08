34deecbd29b0099216e3c14aadc4a49d8ae569b4
Ricardo Polanski <rpolanski@live.com>
2022-08-07 21:21:36 -0300

    Developed deleteComputerActions

---
+++ cypress/integration/tests/actions/deleteComputerActions.js
@@ -0,0 +1,48 @@
+const { DeleteComputerElements } = require('../pageObjects/deleteComputerElements')
+const { CommomElements } = require('../pageObjects/commomElements')
+const { ComputerData } = require('../data/computerInfo')
+const deleteElements = new DeleteComputerElements
+const commomElements = new CommomElements
+const computerData = new ComputerData
+
+exports.DeleteComputerActions = class DeleteComputerActions {
+    constructor(){
+        this.deleleConfirmation = commomElements.confirmationMessage()
+    }
+
+    clickBtnDelete = () => {
+        cy.get(deleteElements.delBtn()).should('be.visible').click()
+    }
+    
+    inputComputerName = () => {
+        cy.get(commomElements.searchElement()).type('ace')
+    }
+    
+    checkResults = () => {
+        cy.get(deleteElements.result()).invoke('text').then((text) => {
+        cy.expect(text).to.equal('6 computers found')
+    })
+    }
+
+    deleteComputer = () => {
+        cy.get(deleteElements.deleteComputer()).find('td').contains(computerData.selectDelComputer()).click()
+    }
+
+    checkDeleteMessage = () => {
+        cy.get(this.deleleConfirmation).and('have.text', `Done !  Computer Acer Extensa 5220 has been deleted`)
+            .and('have.css', 'background-color')
+            .and('eq', 'rgb(238, 220, 148)')
+    }
+}
+
+    
+
+    // selectComputer = () => {
+    //     // common.confirmationMsg()
+    //     // cy.get('@confirmationMsg').and('have.text', `Done !  Computer Acer Extensa 5220 has been deleted`)
+    //     //     .and('have.css', 'background-color')
+    //     //     .and('eq', 'rgb(238, 220, 148)')
+    // }
+           
+
+
---
+++ cypress/integration/tests/features/deleteComputer.feature
@@ -0,0 +1,14 @@
+@deleteComputer
+Feature: Delete Computer after filter
+
+    Feature Description
+
+    Scenario: Delete Computers
+        Given the website is accessed
+        And type into Search Field the Computer name to be deleted
+        When click on the button "Filter by name"
+        Then will be listed computers that match
+        When click on the computer name to be deleted
+        And click on the Delete This Computer button
+        Then a delete confirmation message will be displayed
+           
